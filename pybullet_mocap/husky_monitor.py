d086b73f48b7507aad4b56620e2cb07b319586a9
Jakob Genhart <jakob.genhart@inf.ethz.ch>
2024-10-31 15:36:40 +0100

    setup ros2 package

---
+++ pybullet_mocap/husky_monitor.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from pybullet_mocap.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'pybullet_mocap'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jakob Genhart',
+    maintainer_email='jakob.genhart@inf.ethz.ch',
+    description='Monitor node for husky robots.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'husky_monitor = pybullet_mocap.husky_monitor:main'
+        ],
+    },
+)
