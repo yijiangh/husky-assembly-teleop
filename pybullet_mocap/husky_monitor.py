6973e174aed824f0aa407087c951d0f606513478
Jakob Genhart <jakob.genhart@inf.ethz.ch>
2024-10-31 18:23:22 +0100

    WIP

--- pybullet_mocap/husky_monitor.py
+++ pybullet_mocap/husky_monitor.py
@@ -1,20 +1,47 @@
 import rclpy
+from rclpy.node import Node
 
 import numpy as np
+import pybullet_planning as pp
+import pybullet as p
 
 from pybullet_mocap.husky_robot import HuskyRobotInterface
+    
+class HuskyMonitor(Node):
+    def __init__(self):
+        super().__init__('husky_monitor')
+        
+        self.husky = HuskyRobotInterface(self)
+                
+        self.timer = self.create_timer(0.05, self.timer_callback)
+        
+        self.start_pybullet()
+        
+    def start_pybullet(self):
+        # start pybullet simulator
+        pp.connect(use_gui=True, shadows=True, color=[0.9, 0.9, 1.0])
+        # y-up to be consistent with mocap
+        p.configureDebugVisualizer(p.COV_ENABLE_Y_AXIS_UP, 1, physicsClientId=pp.CLIENT)
+        # turn on the GUI panels
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1, physicsClientId=pp.CLIENT)
+
+        self.param_slider = p.addUserDebugParameter("a slider", 0.0, 1.0, 0.0)
+    
+    def timer_callback(self):
+        #p.addUserDebugText(text="AAAA", textPosition=[0,100,100])
+        p.addUserDebugPoints(pointPositions=[[self.husky.xpos*100,0,0]],pointColorsRGB=[[0.9, 0.9, 0.0]],lifeTime=1.1,pointSize=10)
 
 def main(args=None):
     rclpy.init(args=args)
 
-    husky_robot = HuskyRobotInterface()
+    husky_monitor = HuskyMonitor()
 
-    rclpy.spin(husky_robot)
+    rclpy.spin(husky_monitor)
 
-    husky_robot.destroy_node()
+    husky_monitor.destroy_node()
     rclpy.shutdown()
 
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':     
     main()
--- pybullet_mocap/husky_robot.py
+++ pybullet_mocap/husky_robot.py
@@ -1,57 +1,48 @@
-import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
 from tf2_msgs.msg._tf_message import TFMessage
 from geometry_msgs.msg._transform import Transform
 from geometry_msgs.msg._twist import Twist
 from sensor_msgs.msg._joy import Joy
 
 import numpy as np
-import pybullet_mocap.husky_robot as husky_robot
 
-class HuskyRobotInterface(Node):
-    def __init__(self, name='/a200_0804'):
-        super().__init__('husky_robot_interface')
+class HuskyRobotInterface:
+    xpos = 0
+    
+    def __init__(self, node: Node, name='/a200_0804'):
+        self.node = node
         
         # Listeners --- --- --- --- ---
-        self.sub_tf = self.create_subscription(
+        self.sub_tf = self.node.create_subscription(
             TFMessage,
             name + '/tf',
             self.tf_callback,
             10)
         
-        self.sub_joy = self.create_subscription(
+        self.sub_joy = self.node.create_subscription(
             Joy,
             name + '/joy_teleop/joy',
             self.joy_callback,
             10)
         
         # Publishers --- --- --- --- ---
-        self.pub_cmd_vel = self.create_publisher(Twist, name + '/cmd_vel', 10)
-        
-        # timers --- --- --- --- ---
-        self.timer = self.create_timer(0.5, self.timer_callback)
+        self.pub_cmd_vel = self.node.create_publisher(Twist, name + '/cmd_vel', 10)
         
         # done --- --- --- --- ---
-        self.get_logger().info(f'Husky Monitor startet on "{name}"!')
-        
-        husky_robot.do()
+        self.node.get_logger().info(f'Husky Monitor startet on "{name}"!')
 
     def tf_callback(self, msg: TFMessage):
         ts: Transform = msg.transforms[0].transform
         pos = np.array((ts.translation.x, ts.translation.y, ts.translation.z))
-        self.get_logger().info(f'Position {np.around(pos, decimals=2)}')
+        self.xpos = pos[0]
+        #self.get_logger().info(f'Position {np.around(pos, decimals=2)}')
     
     # for debugging intermittent joy control
     # joy node sometimes periodically sends zero values even tough stick is held continuously...
     def joy_callback(self, msg: Joy):
         pass
-        #self.get_logger().info(f'Velocity {msg}')
-    
-    def timer_callback(self):
-        pass
-        #self.send_cmd_vel(0.0, 0.1)
+        #self.node.get_logger().info(f'Velocity {msg}')
         
     def send_cmd_vel(self, x_dot, theta_dot):
         msg = Twist()
