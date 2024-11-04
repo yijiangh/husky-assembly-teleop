74752bc91c6e779345dbaf02cd01b86844cf26e1
Jakob Genhart <jakob.genhart@inf.ethz.ch>
2024-11-04 11:55:04 +0100

    copy pybullet_mocap & data

---
+++ pybullet_mocap/lib/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+DATA_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
+RECORD_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'recorded_data'))
