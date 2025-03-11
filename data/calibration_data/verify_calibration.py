35b7a55665ea134c8f5ddeb2fc82f7abfc8af462
Yijiang Huang <yijiang.huang@inf.ethz.ch>
2025-03-11 21:33:01 +0100

    verification results

---
+++ data/calibration_data/verify_calibration.py
@@ -0,0 +1,103 @@
+from cProfile import label
+import os, logging
+import json
+import numpy as np
+from skspatial.objects import Line, Point, Vector
+import pybullet_planning as pp
+import matplotlib.pyplot as plt
+# from pybullet_mocap.common import load_robot
+
+HUSKY_UR5e_JOINT_NAMES = ["ur_arm_shoulder_pan_joint", 
+                      "ur_arm_shoulder_lift_joint",
+                      "ur_arm_elbow_joint", 
+                      "ur_arm_wrist_1_joint", 
+                      "ur_arm_wrist_2_joint", 
+                      "ur_arm_wrist_3_joint" ]
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+data_folder = os.path.join(HERE, 'verification')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+
+# Create file handler
+file_handler = logging.FileHandler(os.path.join(data_folder, f'log.txt'))
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+# Add handlers to the logger
+logger.addHandler(file_handler)
+
+pp.connect(use_gui=False, shadows=True, color=[0.9, 0.9, 1.0])
+# robot_urdf = os.path.join('/home/yijiangh/ros2_ws/src/husky-asembly-teleop/data','husky_urdf/mt_husky_moveit_config/urdf/husky_ur5_e_no_base_joint.urdf')
+robot_urdf = os.path.join('/home/yijiangh/ros2_ws/src/pybullet_mocap/data', 'husky_urdf/mt_husky_moveit_config/urdf/husky_ur5_e_no_base_joint.urdf')
+with pp.HideOutput():
+    robot = pp.load_pybullet(robot_urdf, fixed_base=False, cylinder=False)
+
+calib_file_path = os.path.join(HERE, 'calibrated_transformation_0804.json')
+with open(calib_file_path, 'r') as file:
+    data = json.load(file)
+base_mocap_from_base_footprint = data['base_mocap_from_base_footprint']
+
+json_files = [f for f in os.listdir(data_folder) if f.startswith('calibration_') and f.endswith('.json')]
+tool0_from_flange_mocap_batches = []
+for i, file_name in enumerate(json_files):
+    logger.info('Working on file: %s', file_name)
+    file_path = os.path.join(data_folder, file_name)
+
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+
+    for entry in data['raw_data']:
+        flange_mocap_pose = entry.get("flange_mocap_pose", [])
+        base_mocap_pose = entry.get("base_mocap_pose", [])
+        conf = entry.get("joint_conf", [])
+
+        # compute arm FK based on the base_mocap_pose and the calibrated offset
+        world_from_footprint = pp.multiply(base_mocap_pose, base_mocap_from_base_footprint)
+        pp.set_pose(robot, world_from_footprint)
+
+        arm_joints = pp.joints_from_names(robot, HUSKY_UR5e_JOINT_NAMES)
+        pp.set_joint_positions(robot, arm_joints, conf)
+
+        world_from_tool0 = pp.get_link_pose(robot, pp.link_from_name(robot, 'ur_arm_tool0'))
+        tool0_from_flange_mocap = pp.multiply(pp.invert(world_from_tool0), flange_mocap_pose)
+
+        tool0_from_flange_mocap_batches.append(tool0_from_flange_mocap) 
+
+        pp.draw_pose(world_from_tool0)
+        pp.draw_pose(flange_mocap_pose)
+        pp.draw_pose(tool0_from_flange_mocap)
+
+pp.wait_if_gui()
+
+origins = [pose[0] for pose in tool0_from_flange_mocap_batches]
+origin_mean = np.mean(origins, axis=0)
+# compute distance to mean, scale to mm
+distances = [1e3 * np.linalg.norm(origin - origin_mean) for origin in origins]
+logger.info('Max origin distance to mean: %f', max(distances))
+
+# plot distance in a line plot
+plt.plot(distances, label='origin to mean distance (mm)')
+
+tfs = [pp.matrix_from_quat(pose[1]) for pose in tool0_from_flange_mocap_batches]
+for i in range(3):
+    x_axes = [tf[:3,i] for tf in tfs]
+    # compute angle between each axis and the mean
+    x_axis_mean = np.mean(x_axes, axis=0)
+    x_axis_mean = x_axis_mean / np.linalg.norm(x_axis_mean)
+    x_angles = []
+    for j in range(len(tfs)-1):
+        angle = np.rad2deg(np.arccos(np.dot(x_axes[j], x_axis_mean)))
+        x_angles.append(angle)
+
+    logger.info(f'Axis {i}')
+    logger.info('Max axis angle to mean vec: %f', np.mean(x_angles))
+    plt.plot(x_angles, label=f'axis {i} to mean angle (deg)')
+
+plt.legend()
+plt.show()
+plt.savefig(os.path.join(data_folder, 'verification.png'))
+
+pp.disconnect()
