c622e7da9b10b0aa93492e9451741eeb38946385
Jakob Genhart <jgenhart@student.ethz.ch>
2025-05-26 19:09:32 +0200

    improve analysis script

--- data/dual_arm_acc_data/0_dual_arm_acc_data_processing.py
+++ data/dual_arm_acc_data/0_dual_arm_acc_data_processing.py
@@ -6,6 +6,7 @@
 import json, os
 import logging, datetime
 import matplotlib.pyplot as plt
+from scipy.spatial.transform import Rotation as R
 import numpy as np
 import pybullet_planning as pp
 
@@ -16,7 +17,8 @@ MARKER_NAME_PAIRS = [
     ['1', '3']
 ]
 
-DATA_BATCH = '20250509'
+#DATA_BATCH = '20250509'
+DATA_BATCH = '20250516'
 EXPORT = 1
 
 
@@ -55,7 +57,8 @@ for i, file_name in enumerate(json_files):
 
     # TODO fix hndling of multiple files
     
-    offset_data = []
+    pos_offset_data = []
+    rot_offset_data = []
     # compute offset data
     for entry in data['raw_data']:
         left_EE_pose = entry.get("left_EE_pose", [])
@@ -64,14 +67,38 @@ for i, file_name in enumerate(json_files):
         left_EE_position = np.array(left_EE_pose[0])
         right_EE_position = np.array(right_EE_pose[0])
         
-        offset = left_EE_position - right_EE_position
-        offset_data.append(np.linalg.norm(offset))
+        pos_offset = left_EE_position - right_EE_position
+        pos_offset_data.append(np.linalg.norm(pos_offset))
+
+        left_EE_rot = R.from_quat(left_EE_pose[1])
+        right_EE_rot = R.from_quat(right_EE_pose[1])
+ 
+        rot_offset = right_EE_rot.inv() * left_EE_rot
+        rot_offset_data.append(rot_offset.as_euler("xyz", degrees=True))
     
-    mean = np.mean(offset_data)
-    offset_data -= mean
+    mean_pos_offset = np.mean(pos_offset_data)
+    logger.info('Mean positional offset %.4f', mean_pos_offset)
+    pos_offset_data -= mean_pos_offset
+
+    mean_rot_offset = np.mean(rot_offset_data, axis=0)
+    logger.info('Mean rotational offset %.4f %.4f %.4f', *mean_rot_offset)
+    rot_offset_data -= mean_rot_offset
     
-    ax = fig.add_subplot()
-    ax.plot(offset_data)
+    axp = fig.add_subplot(2,1, 1)
+
+    axp.plot(pos_offset_data)
+    axp.set_title(f'Positional Offset {i+1}')
+    axp.set_xlabel('Sample Index')
+    axp.set_ylabel('Offset (m)')
+    axp.grid(True)
+
+    axr = fig.add_subplot(2,1, 2)
+    axr.plot(rot_offset_data)
+    axr.set_title(f'Rotational Offset {i+1}')
+    axr.set_xlabel('Sample Index')
+    axr.set_ylabel('Offset (deg)')
+    axr.legend(['X', 'Y', 'Z'])
+    axr.grid(True)
 
 if EXPORT:
     plt.savefig(os.path.join(data_folder, f'dual_arm_acc_{DATA_BATCH}.png'))
