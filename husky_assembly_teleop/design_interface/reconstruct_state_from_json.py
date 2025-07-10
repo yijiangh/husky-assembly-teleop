ffa4c6374f101381f6d761bf5ed88fec8a1e424c
Yijiang Huang <yijiang94817@gmail.com>
2025-07-10 16:04:34 +0200

    example GH + python scripts for Zihao for generating and parsing a pair of states for planning

--- husky_assembly_teleop/design_interface/reconstruct_state_from_json.py
+++ husky_assembly_teleop/design_interface/reconstruct_state_from_json.py
@@ -17,14 +17,19 @@ robot_cell_json_path = os.path.join(
 with open(robot_cell_json_path, "r") as f:
     robot_cell = json_load(f)
 
-robot_cell_state_path = os.path.join(
+robot_cell_state_folder = os.path.join(
     design_study_path,
     design_case,
     "RobotCellStates",
-    "robotx_box_A0-G_RobotCellState.json"
 )
-with open(robot_cell_state_path, "r") as f:
-    robot_cell_state = json_load(f)
+start_state_file = os.path.join(robot_cell_state_folder, "robotx_box_A0-G_start_state_RobotCellState.json")
+end_state_file = os.path.join(robot_cell_state_folder, "robotx_box_A0-G_RobotCellState.json")
+
+with open(start_state_file, "r") as f:
+    start_state = json_load(f)
+
+with open(end_state_file, "r") as f:
+    end_state = json_load(f)
 
 # --- 5. Run IK using PyBullet ---
 use_gui = True
@@ -36,19 +41,27 @@ with PyBulletClient(connection_type="gui" if use_gui else "direct", verbose=Fals
     start = time.time()
     with pp.LockRenderer(0):
         planner.set_robot_cell(robot_cell)
-        planner.set_robot_cell_state(robot_cell_state)
+        planner.set_robot_cell_state(start_state)
     print("Setting robot cell and state took {:.3f} seconds".format(time.time() - start))
 
     # Robot base pose need to be manually updated
-    robot_base_frame = robot_cell_state.robot_base_frame
+    robot_base_frame = start_state.robot_base_frame
     robot_base_pose = pose_from_frame(robot_base_frame)
     pp.set_pose(client.robot_puid, robot_base_pose)
+
+    pp.wait_if_gui('start state')
 
     # ! IK targets are saved in the file called 'robotx_box_A0-G_GraspTargets.json'
     # for parsing, see load_grasp_targets.py
 
-    # pose = pose_from_frame(frame_WCF)
-    # pp.draw_pose(pose)
-    pp.wait_if_gui()
+    planner.set_robot_cell_state(end_state)
+
+    # Robot base pose need to be manually updated
+    robot_base_frame = end_state.robot_base_frame
+    robot_base_pose = pose_from_frame(robot_base_frame)
+    pp.set_pose(client.robot_puid, robot_base_pose)
+
+    pp.wait_if_gui('end state')
+
 
 print("Done.") 
